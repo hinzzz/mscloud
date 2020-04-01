c1f9c171a2baaee15f07b0f8642a3f1757148602
hinzzz <157957329@qq.com>
2020-04-01 17:48:13 +0800

    添加zookeeper服务端

--- cloud-provider-payment8001/src/main/java/com/hinz/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/hinz/controller/PaymentController.java
@@ -5,6 +5,8 @@ import com.hinz.entities.Payment;
 import com.hinz.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,10 +21,26 @@ public class PaymentController
     @Resource
     private PaymentService paymentService;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @Value("${server.port}")
     private String serverPort;
 
 
+
+    @GetMapping("/payment/dicovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment)
